fb259baa01ed044136ef290c777182a21c21e437
Islam <khasievislamcloud@gmail.com>
2023-03-15 15:51:50 +0300

    коммит е бох перед стягиванием

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import styles from "./Header.module.css"
-import vector1 from "..//..//assets/Vector.png"
-import vector2 from "..//..//assets/Vector2.png"
-import basket from "..//..//assets/basket.png"
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import styles from "./Header.module.css";
+import vector1 from "..//..//assets/Vector.png";
+import vector2 from "..//..//assets/Vector2.png";
+import basket from "..//..//assets/basket.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className={styles.header}>
-            <div className={styles.headerLogo}>
-                <img src={vector1} alt="vector" />  
-                <a href="#">Womazing</a> 
-            </div>
-            <div className={styles.headerLick}>  
-                <NavLink to={''}>Главная</NavLink>
-                <NavLink to={''}>Магазин</NavLink>
-                <NavLink to={''}>О бренде</NavLink>
-                <NavLink to={''}>Контакты</NavLink>
-            </div>
-            <div className={styles.headersContact}>
-                <img src={vector2} alt="phone" />
-                <div className={styles.headerNumber}>+7 (495) 823-54-12</div>
-                <div className={styles.headerBasket}><a href="#"><img src={basket} alt="" /></a></div>
-            </div>
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerLogo}>
+        <img src={vector1} alt='vector' />
+        <a href='#'>Womazing</a>
+      </div>
+      <div className={styles.headerLick}>
+        <NavLink to={""}>Главная</NavLink>
+        <NavLink to={""}>Магазин</NavLink>
+        <NavLink to={""}>О бренде</NavLink>
+        <NavLink to={""}>Контакты</NavLink>
+      </div>
+      <div className={styles.headersContact}>
+        <img src={vector2} alt='phone' />
+        <div className={styles.headerNumber}>+7 (495) 823-54-12</div>
+        <div className={styles.headerBasket}>
+          <a href='#'>
+            <img src={basket} alt='' />
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
