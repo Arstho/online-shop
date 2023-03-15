6a04f8cbb472a8dcede1d1538624804e2fa458cc
Islam <khasievislamcloud@gmail.com>
2023-03-15 15:12:39 +0300

    Авторизация на фронте V1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import { Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Checkout from './pages/СheckoutPage';
-
-function App() {
-  return (
-     <div className='App'>
-       <Header/>
-       <Checkout/>
-=======
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -21,33 +8,46 @@ import Home from "./pages/Home/Home";
 import Success from "./pages/Success/Success";
 import ShopPage from "./pages/ShopPage/ShopPage";
 import OnePage from "./pages/OnePage/OnePage";
-import { Register } from './pages/Register/Register'
-import { Login } from './pages/Login/Login'
+import { Register } from "./pages/Register/Register";
+import { Login } from "./pages/Login/Login";
+import { useSelector } from "react-redux";
 
 function App() {
+  const token = useSelector((state) => state.authReducer.token);
+
+  if (!token) {
+    return (
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/success' element={<Success />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/el' element={<OnePage />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    );
+  }
   return (
     <div className='App'>
-      <Header/>
->>>>>>> master
+      <Header />
       <Routes>
         <Route path='/cart' element={<Cart />} />
         <Route path='/contact' element={<ContactPage />} />
         <Route path='/' element={<Home />} />
-        <Route path='/success' element={<Success/>}/>
+        <Route path='/success' element={<Success />} />
         <Route path='/shop' element={<ShopPage />} />
         <Route path='/el' element={<OnePage />} />
-        <Route path='register' element={<Register />} />
-        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Navigate to='/' />} />
+        <Route path='login' element={<Navigate to='/' />} />
       </Routes>
-<<<<<<< HEAD
-       <Footer/> 
-
-     </div>
-       
-=======
-      <Footer/>
+      <Footer />
     </div>
->>>>>>> master
   );
 }
 
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import categoriesSlice from "../features/categorySlice";
+import authReducer from "../features/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    // categories: categoriesSlice,
+    authReducer,
+  },
+});
