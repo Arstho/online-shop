3a429a730a2a208adc5528cc4407a0628056d0fb
magoador <magomedov951.mail.ru>
2023-03-16 15:26:56 +0300

    подправил верстку в ShopPage сделал переход при клике на карточку

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -9,11 +9,11 @@ import { Link, NavLink, useParams } from "react-router-dom";
 const ShopPage = () => {
   const { categoryId } = useParams();
 
-  const categories = useSelector((state) => state.categories.categories);  
+  const categories = useSelector((state) => state.categories.categories);
   const clothes = useSelector((state) =>
-    state.clothes.clothes.filter((elem) => { 
-      if (!categoryId) return true
-      return elem.category === categoryId;  
+    state.clothes.clothes.filter((elem) => {
+      if (!categoryId) return true;
+      return elem.category === categoryId;
     })
   );
 
@@ -21,41 +21,61 @@ const ShopPage = () => {
 
   useEffect(() => {
     dispatch(fetchClothes());
-  }, []);
-
-  useEffect(() => {
     dispatch(fetchCategories());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className={styles.ShopPage}>
       <div className={styles.bText}>Магазин</div>
       <div className={styles.linkToThisPage}>
-        Главная <span className={styles.defis}>—</span>{" "}  
-        <span className={styles.opacityText}>Магазин</span>   
+        Главная <span className={styles.defis}>—</span>{" "}
+        <span className={styles.opacityText}>Магазин</span>
       </div>
       <div className={styles.categories_row}>
         <div className={styles.category}>
-          <NavLink to={'/shop'} className={({ isActive }) => (isActive ? styles.category_active : styles.category)}><button>Все</button></NavLink>  
+          <NavLink
+            to={"/shop"}
+            className={({ isActive }) =>
+              isActive ? styles.category_active : styles.category
+            }
+          >
+            <button>Все</button>
+          </NavLink>
         </div>
         {categories.map((category) => {
           return (
             <div className={styles.category}>
-              <NavLink to={`/category/${category._id}`} className={({ isActive }) => (isActive ? styles.category_active : styles.category)}><button>{category.category}</button></NavLink>  
+              <NavLink
+                to={`/category/${category._id}`}
+                className={({ isActive }) =>
+                  isActive ? styles.category_active : styles.category
+                }
+              >
+                <button>{category.category}</button>
+              </NavLink>
             </div>
           );
         })}
       </div>
-      <div className={styles.countOfVisibleItems}>   
+      <div className={styles.countOfVisibleItems}>
         Показано: 9 из 12 товаров
       </div>
       <div className={styles.items}>
         {clothes.map((carts) => {
           return (
-            <div className={styles.carts}>
-              <img src={`http://localhost:4000/${carts.image}`} />   
-              <div className={styles.name}>{carts.name}</div>
-              <div className={styles.price}>${carts.price}</div>  
+            <div className={styles.item}>
+              <Link to={`/el/${carts._id}`}>
+                <div className={styles.colletcionItemHover}></div>
+              </Link>
+              <img
+                src={`http://localhost:4000/${carts.image}`}
+                className={styles.item_img}
+              />
+              <div className={styles.item_name}>{carts.name}</div>
+              <div className={styles.item_priceRow}>
+                <div className={styles.priceRow_oldPrice}></div>
+                <div className={styles.priceRow_newPrice}>${carts.price}</div>
+              </div>
             </div>
           );
         })}
